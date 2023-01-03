5ab8494ef6702f2ebd35b0111d57407e17ba6972
NguyenVu <vunq20@wru.vn>
2023-01-03 16:21:25 +0700

    Chapter 3

--- Vocabulary/Program.cs
+++ Vocabulary/Program.cs
@@ -6,28 +6,14 @@ namespace Vocabulary
     {
         static void Main(string[] args)
         {
-            Assembly? myApp = Assembly.GetEntryAssembly();
-            if (myApp == null) return; // quit the app
-                                       // loop through the assemblies that my app references
-            foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+            string? password;
+            do
             {
-                // load the assembly so we can read its details
-                Assembly a = Assembly.Load(name);
-                // declare a variable to count the number of methods
-                int methodCount = 0;
-                // loop through all the types in the assembly
-                foreach (TypeInfo t in a.DefinedTypes)
-                {
-                    // add up the counts of methods
-                    methodCount += t.GetMethods().Count();
-                }
-                // output the count of types and their methods
-                Console.WriteLine(
-                "{0:N0} types with {1:N0} methods in {2} assembly.",
-                arg0: a.DefinedTypes.Count(),
-                arg1: methodCount,
-                arg2: name.Name);
+                Write("Enter your password: ");
+                password = ReadLine();
             }
+            while (password != "Pa$$w0rd");
+            WriteLine("Correct!");
         }
     }
 }
